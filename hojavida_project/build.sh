f1326720ef7dcbcec9b7c5c93bce1c704fcfa913
Rubén <jorgemerofranco@gmail.com>
2026-01-21 09:17:12 -0500

    Auto-crear usuario en el build

--- hojavida_project/build.sh
+++ hojavida_project/build.sh
@@ -1,4 +1,15 @@
 #!/usr/bin/env bash
+set -o errexit
+
 pip install -r requirements.txt
-python manage.py collectstatic --noinput
+python manage.py collectstatic --no-input
 python manage.py migrate
+
+# ESTA LÍNEA CREA TU USUARIO SI NO EXISTE
+python manage.py shell << END
+from django.contrib.auth import get_user_model
+User = get_user_model()
+if not User.objects.filter(username='jsrubenmerofranco').exists():
+    User.objects.create_superuser('jsrubenmerofranco', '', '12345678')
+    print('Superusuario creado exitosamente')
+END
