bbfc0846e25d53d37aabdfe0b7a1e088d9b528f8
Rubén <jorgemerofranco@gmail.com>
2026-01-21 08:28:07 -0500

    build.sh

--- hojavida_project/hojavida_project/settings.py
+++ hojavida_project/hojavida_project/settings.py
@@ -126,9 +126,9 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
-if not Debug:
-
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+if not DEBUG:
+    
+    STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
